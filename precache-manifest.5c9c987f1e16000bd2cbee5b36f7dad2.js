6ad384596adc6016ef9b427462434fc25bb8b6fb
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-10-06 16:37:38 +0900

    Updates

---
+++ precache-manifest.5c9c987f1e16000bd2cbee5b36f7dad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d465b2ec3753163d4a08a2a084fcc87",
+    "url": "/A2B2_login/index.html"
+  },
+  {
+    "revision": "2c55779cb2da6f3c0040",
+    "url": "/A2B2_login/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "74793cb09fb6b13b4fce",
+    "url": "/A2B2_login/static/css/main.4ead9fd2.chunk.css"
+  },
+  {
+    "revision": "2c55779cb2da6f3c0040",
+    "url": "/A2B2_login/static/js/2.8caf0c9d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/A2B2_login/static/js/2.8caf0c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74793cb09fb6b13b4fce",
+    "url": "/A2B2_login/static/js/main.9a41bef6.chunk.js"
+  },
+  {
+    "revision": "83a8511cc1dc8ea61058",
+    "url": "/A2B2_login/static/js/runtime-main.3a4ffb9e.js"
+  }
+]);
