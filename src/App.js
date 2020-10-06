bea863167147e75c2d119397f464686b1599f0bc
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-10-06 16:32:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { FacebookLoginButton, TwitterLoginButton } from 'react-social-login-buttons';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form">
+      <h1>
+        <span className="font-weight-bold">FANDING</span>
+      </h1>
+      <h2 className="text-center">Welcome</h2>
+      <FormGroup>
+        <Label>이메일</Label>
+        <Input type="email" placeholder="Email"/>
+      </FormGroup>
+      <FormGroup>
+        <Label>비밀번호</Label>
+        <Input type="password" placeholder="Password"/>
+      </FormGroup>
+      <Button className="btn-lg btn-block" color="warning">
+        <b>로그인</b>
+      </Button>
+      <div className="text-center pt-3">
+        Or continue with your social account
+      </div>
+      <TwitterLoginButton className="mt-3 mb-3"/>
+      <div className="text-center">
+        <a href="/sign-up">비밀번호</a>
+        <span className="p-2">|</span>
+        <a href="/forgot-password">비밀번호 찾기</a>
+      </div>
+    </Form>
   );
 }
 
